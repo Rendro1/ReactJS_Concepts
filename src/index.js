f9e733e8be25ae127e27eda1c6382579573f4d25
Arka Mukherjee <85149698+Rendro1@users.noreply.github.com>
2024-04-29 13:09:05 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let currDate = new Date(2023,10,25,8);
+currDate = currDate.getHours();
+let greet;
+const cssStyle = {
+  color:'green'
+}
 
+if(currDate>=1 && currDate<12){
+  greet = " Good Morning!";
+  cssStyle.color="green";
+}
+else if(currDate>=12 && currDate<19){
+  greet = " Good AfterNoon!";
+  cssStyle.color="orange";
+}
+else{
+  greet = " Good Night!";
+  cssStyle.color="black";
+}
+
+
+
+ReactDOM.render(
+  <h1>Hii Arka, <span style={cssStyle}>{greet}</span></h1>,
+  document.getElementById('root')
+); 
